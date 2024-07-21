20eaaf2eef3077351009aeda82c9f90856dbb5c0
Hoang Tran <descience.thh10@gmail.com>
2024-07-21 15:15:50 +0700

    add a playground notebook

---
+++ metrics.py
@@ -0,0 +1,9 @@
+from fol_parser import parse_text_FOL_to_tree
+
+def is_syntactically_valid(formula: str) -> bool:
+    root_node = parse_text_FOL_to_tree(formula)
+    return root_node is not None
+
+def is_execution_accurate() -> bool:
+    # TODO:
+    pass
