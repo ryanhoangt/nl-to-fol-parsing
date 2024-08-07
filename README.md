00c606b9d60634f9c6c13810b395cb8207aac26a
Hoang Tran <descience.thh10@gmail.com>
2024-08-07 12:13:40 +0700

    add inference notebook to run a single test instance

--- README.md
+++ README.md
@@ -2,15 +2,21 @@
 
 ## Setup
 
+1. Create a new conda environment and install the required packages.
+
 ```bash
 conda create -n nl2fol-local python=3.8
 conda activate nl2fol-local
 pip install -r requirements.txt
 ```
 
+2. Setup the environment variables.
+
+Create a `.env` file in the root directory based on the `.env.template` file.
+
 ## Usage
 
-<!-- TODO: -->
+Use the notebook `translate.ipynb` to translate natural language to first-order logic.
 
 ## Metrics
 
---
+++ prompt/__init__.py
@@ -0,0 +1,17 @@
+import os
+from utils import load_file
+
+PROMPT_DIR = os.path.join(os.path.dirname(__file__), "templates")
+EXAMPLES_DIR = os.path.join(os.path.dirname(__file__), "in_context_examples")
+TRANSLATE_TEMPLATE_V1 = load_file(os.path.join(PROMPT_DIR, "translate_v1.txt"))
+TRANSLATE_EXAMPLES_V1 = load_file(os.path.join(EXAMPLES_DIR, "translate_v1.txt"))
+
+
+class PromptTemplate:
+    """A prompt template."""
+
+    def __init__(self, template: str):
+        self.template: str = template
+
+    def __call__(self, **kwargs) -> str:
+        return self.template.format(**kwargs)
